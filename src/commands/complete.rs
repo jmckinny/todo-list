0f883c577bb860d9930e5335e20541687287b5c1
Justin McKinney <justinmckinney.md@gmail.com>
2024-12-30 17:24:06 -0500

    fix: format

--- src/commands/complete.rs
+++ src/commands/complete.rs
@@ -1,7 +1,6 @@
+use super::utils::get_index;
 use crate::data::todo_list::TodoList;
 use crate::TodoError;
-
-use super::utils::get_index;
 
 pub fn complete_item_index(todo_list: &mut TodoList, args: &[String]) -> Result<(), TodoError> {
     let index = get_index(args)?;
