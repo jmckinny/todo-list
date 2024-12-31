d2222c06a32b061ea6fae45d6ee7d660543dc17d
Justin McKinney <justinmckinney.md@gmail.com>
2024-12-31 00:58:31 -0500

    fix: spacing

--- src/commands/help.rs
+++ src/commands/help.rs
@@ -5,8 +5,8 @@ pub fn print_help() {
     println!("  help                                Show help message");
     println!("  list                                List todo items (default command)");
     println!("  add <content>                       Add item to todo list");
-    println!("  remove <index | fuzzy_match>        Remove item from list");
-    println!("  complete <index | fuzzy_match>      Mark item complete on list");
-    println!("  uncomplete <index | fuzzy_match>    Mark item uncomplete on list");
+    println!("  remove <index|fuzzy_search>         Remove item from list");
+    println!("  complete <index|fuzzy_search>       Mark item complete on list");
+    println!("  uncomplete <index|fuzzy_search>     Mark item uncomplete on list");
     println!("  clean                               Remove completed items");
 }
